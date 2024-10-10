97441c17062cf17a190e6a08b235a09ee199558d
Dyllon Dekok <dyllon.dekok1@gmail.com>
2024-10-10 11:40:44 -0400

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebAppFinalProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
